9817c82f5d8ef5596020aac9aa0de794d23e7295
Tushita Gupta <guptatushita5@gmail.com>
2018-10-22 08:56:42 -0700

    adding initial speech processing file

---
+++ speech_processing.py
@@ -0,0 +1,39 @@
+import speech_recognition as sr
+
+recognizer = sr.Recognizer()
+microphone = sr.Microphone()
+# microphone = sr.Microphone(device_index=1)
+
+if not isinstance(recognizer, sr.Recognizer):
+    raise TypeError("`recognizer` must be `Recognizer` instance")
+
+if not isinstance(microphone, sr.Microphone):
+    raise TypeError("`microphone` must be `Microphone` instance")
+
+# adjust the recognizer sensitivity to ambient noise and record audio
+# from the microphone
+with microphone as source:
+    recognizer.adjust_for_ambient_noise(source)
+    audio = recognizer.listen(source)
+
+# set up the response object
+response = {
+    "success": True,
+    "error": None,
+    "transcription": None
+}
+
+# try recognizing the speech in the recording
+# if a RequestError or UnknownValueError exception is caught,
+#     update the response object accordingly
+try:
+    response["transcription"] = recognizer.recognize_google(audio)
+except sr.RequestError:
+    # API was unreachable or unresponsive
+    response["success"] = False
+    response["error"] = "API unavailable"
+except sr.UnknownValueError:
+    # speech was unintelligible
+    response["error"] = "Unable to recognize speech"
+
+print(response)
