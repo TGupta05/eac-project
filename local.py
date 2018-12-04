ca697a428e21e5e1a8fbf77ff654c1feed1447f8
Tushita Gupta <guptatushita5@gmail.com>
2018-12-03 20:43:09 -0500

    added a file saving mechanism for the results

--- local.py
+++ local.py
@@ -1,51 +1,59 @@
 import speech_recognition as sr
 import time
 import numpy as np
+import sys
 
-recognizer = sr.Recognizer()
-microphone = sr.Microphone()
+def main():
+    recognizer = sr.Recognizer()
+    microphone = sr.Microphone()
 
-if not isinstance(recognizer, sr.Recognizer):
-    raise TypeError("`recognizer` must be `Recognizer` instance")
+    if not isinstance(recognizer, sr.Recognizer):
+        raise TypeError("`recognizer` must be `Recognizer` instance")
 
-if not isinstance(microphone, sr.Microphone):
-    raise TypeError("`microphone` must be `Microphone` instance")
+    if not isinstance(microphone, sr.Microphone):
+        raise TypeError("`microphone` must be `Microphone` instance")
 
-# adjust the recognizer sensitivity to ambient noise and record audio
-# from the microphone
-# audio.frame_data, audio.sample_rate, audio.sample_width
-local_start_time = time.time()
-with microphone as source:
-    recognizer.adjust_for_ambient_noise(source)
-    audio = recognizer.listen(source)
-local_end_time = time.time()
-print("Elapsed time for local computation = " + str(local_end_time-local_start_time))
+    # adjust the recognizer sensitivity to ambient noise and record audio
+    # from the microphone
+    # audio.frame_data, audio.sample_rate, audio.sample_width
+    local_start_time = time.time()
+    with microphone as source:
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+    local_end_time = time.time()
+    print("Elapsed time for local computation = " + str(local_end_time-local_start_time))
 
-saving_start_time = time.time()
-np.save("audio.npy", audio.frame_data)
-np.save("sample_rate.npy", audio.sample_rate)
-np.save("sample_width.npy", audio.sample_width)
-saving_end_time = time.time()
-print("Elapsed time for saving data = " + str(saving_end_time-saving_start_time))
+    # set up the response object
+    response = {
+        "success": True,
+        "error": None,
+        "transcription": None
+    }
 
-# set up the response object
-response = {
-    "success": True,
-    "error": None,
-    "transcription": None
-}
+    # try recognizing the speech in the recording
+    # if a RequestError or UnknownValueError exception is caught,
+    #     update the response object accordingly
+    try:
+        response["transcription"] = recognizer.recognize_sphinx(audio)
+    except sr.RequestError:
+        # API was unreachable or unresponsive
+        response["success"] = False
+        response["error"] = "API unavailable"
+    except sr.UnknownValueError:
+        # speech was unintelligible
+        response["error"] = "Unable to recognize speech"
 
-# try recognizing the speech in the recording
-# if a RequestError or UnknownValueError exception is caught,
-#     update the response object accordingly
-try:
-    response["transcription"] = recognizer.recognize_sphinx(audio)
-except sr.RequestError:
-    # API was unreachable or unresponsive
-    response["success"] = False
-    response["error"] = "API unavailable"
-except sr.UnknownValueError:
-    # speech was unintelligible
-    response["error"] = "Unable to recognize speech"
+    print(response['transcription'])
 
-print(response['transcription'])
+    saving_start_time = time.time()
+    np.save("audio.npy", audio.frame_data)
+    np.save("sample_rate.npy", audio.sample_rate)
+    np.save("sample_width.npy", audio.sample_width)
+    with open(str(sys.argv[1]), "w+") as d:
+        d.write(response['transcription'])
+
+    saving_end_time = time.time()
+    print("Elapsed time for saving data = " + str(saving_end_time-saving_start_time))
+
+if __name__ == '__main__':
+    main()
